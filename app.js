d3e8e05a8232f9a9f932ddd6feae92700bf0ee92
LibLogic <hdgknsn@gmail.com>
2019-01-24 01:40:19 +0000

    creating a directory asynchronously, then reading a file asynchronously, then writing a file and putting in the directory that was just created

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var fs = require('fs');
 
-fs.readFile('./read.txt', 'utf8', function(err, data){
-	if(err){console.log(err)}
-	fs.writeFile('writeMe.txt', data, function(err){
+fs.mkdir('stuff', function(){
+	fs.readFile('read.txt', 'utf8', function(err, data){
 		if(err){console.log(err)}
+		fs.writeFile('./stuff/writeFile.txt', data, function(err){
+			if(err){console.log(err)}
+		})
 	});
 });
