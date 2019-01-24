d0624e136089fcd9999ccffaefc44ccd2ccf2a68
LibLogic <hdgknsn@gmail.com>
2019-01-24 00:44:23 +0000

    example #2: reading a file asynchonously and writing the results asynchronously immediately within the readFile callback function

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
 
-var readMe = fs.readFileSync('read.txt', 'utf8');
-
-	
-console.log(readMe);
+fs.readFile('./read.txt', 'utf8', function(err, data){
+	if(err){console.log(err)}
+	fs.writeFile('writeMe.txt', data, function(err){
+		if(err){console.log(err)}
+	});
+});
