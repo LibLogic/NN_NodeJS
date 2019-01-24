cee0a03db7a4e5d6362151f748f10e0a4d29fc5d
LibLogic <hdgknsn@gmail.com>
2019-01-24 00:21:55 +0000

    using fs.readFileSync() to read a text file

--- app.js
+++ app.js
@@ -1,24 +1,6 @@
-var events = require('events');
-var util = require('util');
+var fs = require('fs');
 
-var Person = function(name){
-	this.name = name;
-}
+var readMe = fs.readFileSync('read.txt', 'utf8');
 
-util.inherits(Person, events.EventEmitter);
-
-var james = new Person('james');
-var mary = new Person('mary');
-var ryu = new Person('ryu');
-
-var people = [james, mary, ryu];
-
-people.forEach(function(person){
-	person.on('speak', function(mssg){
-		console.log(this.name, 'said:', mssg);
-	});
-});
-
-james.emit('speak', 'hey dudes');
-mary.emit('speak', 'hello');
-ryu.emit('speak', 'I want a coffee');
+	
+console.log(readMe);
